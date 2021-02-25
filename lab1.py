a6727c830f74eaf748313c36cecfd53b03313361
Fulliton <artyshko.andrey@gmail.com>
2021-02-25 22:47:31 +0700

    lab1

---
+++ lab1.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+
+class lab1():
+  """docstring for lab1"""
+  def lab (self, day, month, day1, month1, year1):
+    upperYear = 0
+    format = "%Y-%m-%d"
+
+    base = datetime.strptime(str(year1) + '-' + str(month) + '-' + str(day) , format)
+
+    diff = datetime.strptime(str(year1) + '-' + str(month1) + '-' + str(day1), format) - base
+
+    answer = int(diff.days)
+
+    answer = answer * (-1)
+
+
+    # return answer
+    if answer < 0:
+      base = datetime.strptime(str(year1 + 1) + '-' + str(month) + '-' + str(day) , format)
+      diff = base - datetime.strptime(str(year1) + '-' + str(month1) + '-' + str(day1), format)
+      answer = int(diff.days)
+
+    return answer
+    
+
+
+# if __name__ == '__main__':
+#   day, month = map(int, input().split())
+#   day1, month1, year1 = map(int, input().split())
+
+#   print(lab(day, month, day1, month1, year1))
---
+++ testLab1.py
@@ -0,0 +1,19 @@
+import unittest
+from lab1 import lab1
+
+class TestLab1(unittest.TestCase):
+
+  def test_day(self):
+    lab = lab1()
+    self.assertEqual(lab.lab(25, 12, 24, 12, 2000), 1)
+
+  def test_nextBirthday(self):
+    lab = lab1()
+    self.assertEqual(lab.lab(24, 12, 25, 12, 2000), 364)
+
+  def test_typeError(self):
+    lab = lab1()
+    self.assertRaises(ValueError, lab.lab, 242, 12, 25, 12, 2000)
+
+if __name__ == '__main__':
+    unittest.main()
